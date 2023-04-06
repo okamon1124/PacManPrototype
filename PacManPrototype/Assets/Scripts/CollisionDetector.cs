2d527f7d4f837c1a73d4d154d7e1e6036a34e330
okamon1124 <chungkai1124@gmail.com>
2023-04-06 10:52:00 +0800

    1. Add "Collision Detector" script to check if player touch enemy. 2. Show "GameOver" UI when player touch enemy.

---
+++ PacManPrototype/Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            GameManager.instance.GameOver();
+        }
+    }
+}
--- PacManPrototype/Assets/Scripts/GameManager.cs
+++ PacManPrototype/Assets/Scripts/GameManager.cs
@@ -1,13 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
-    //[SerializeField] GameObject PlayerGameObject;
-    //
-    //private void Awake()
-    //{
-    //    Instantiate(PlayerGameObject, new Vector3(0f, 0.5f, 3f), Quaternion.identity);
-    //}
+    //Instantiate(PlayerGameObject, new Vector3(0f, 0.5f, 3f), Quaternion.identity);
+
+    [SerializeField] GameObject GameOverUIpanel;
+    [SerializeField] GameObject PlayerGameObject;
+    [SerializeField] GameObject[] EnemyGameObjects;
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        instance= this;
+    }
+
+    private void OnDestroy()
+    {
+        if (instance != null)
+        {
+            Destroy(instance);
+        }
+    }
+
+    public void GameOver()
+    {
+        GameOverUIpanel.SetActive(true);
+
+        for (int i = 0;i < EnemyGameObjects.Length;i++)
+        {
+            Destroy(EnemyGameObjects[i]);
+        }
+
+        Destroy(PlayerGameObject);
+    }
+
+
 }
