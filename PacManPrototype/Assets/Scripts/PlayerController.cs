aa02e534cf18dc11ba0bd1ef0664b9849ae2638e
okamon1124 <chungkai1124@gmail.com>
2023-04-11 14:11:34 +0800

    Round the position value when making a turn.

--- PacManPrototype/Assets/Scripts/PlayerController.cs
+++ PacManPrototype/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerController : Character
 {
     private MoveDirection current_direction = MoveDirection.Right;
     private MoveDirection input_direction = MoveDirection.Right;
     [SerializeField] float movement_speed = 1f;
+    
+
 
     private void Start()
     {
-        
+        RoundPosition();
+        //print(pos);
     }
 
     private void Update()
@@ -47,6 +51,10 @@ public class PlayerController : Character
             if(!Physics.CheckSphere(transform.position + new Vector3(0f, 0f, 1f), 0.4f))
             {
                 current_direction = MoveDirection.Forward;
+                if(current_direction != input_direction)
+                {
+                    RoundPosition();
+                }
             }
         }
         else if (input_direction == MoveDirection.Left)
@@ -54,6 +62,10 @@ public class PlayerController : Character
             if (!Physics.CheckSphere(transform.position + new Vector3(-1f, 0f, 0f), 0.4f))
             {
                 current_direction = MoveDirection.Left;
+                if (current_direction != input_direction)
+                {
+                    RoundPosition();
+                }
             }
         }
         else if (input_direction == MoveDirection.Backward)
@@ -61,6 +73,10 @@ public class PlayerController : Character
             if (!Physics.CheckSphere(transform.position + new Vector3(0f, 0f, -1f), 0.4f))
             {
                 current_direction = MoveDirection.Backward;
+                if (current_direction != input_direction)
+                {
+                    RoundPosition();
+                }
             }
         }
         else if (input_direction == MoveDirection.Right)
@@ -68,6 +84,10 @@ public class PlayerController : Character
             if (!Physics.CheckSphere(transform.position + new Vector3(1f, 0f, 0f), 0.4f))
             {
                 current_direction = MoveDirection.Right;
+                if (current_direction != input_direction)
+                {
+                    RoundPosition();
+                }
             }
         }
     }
@@ -91,4 +111,18 @@ public class PlayerController : Character
             transform.position += new Vector3(movement_speed * Time.deltaTime, 0f, 0f);
         }
     }
+
+    private void RoundPosition()
+    {
+        double x_pos = Convert.ToDouble(transform.position.x);
+        double y_pos = Convert.ToDouble(transform.position.y);
+        double z_pos = Convert.ToDouble(transform.position.z);
+
+        x_pos = Math.Round(x_pos);
+        y_pos = Math.Round(y_pos);
+        z_pos = Math.Round(z_pos);
+
+        transform.position = new Vector3 ( (float)x_pos, (float)y_pos, (float)z_pos );
+    }
+
 }
